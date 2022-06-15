32e54145617cb6593dad68a859158740a45b7a4a
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-06-14 21:32:50 -0300

    Criando primeiro Controller
    Criando primeiro Controller

--- src/main/java/br/com/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/mvc/mudi/MudiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MudiApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MudiApplication.class, args);
 	}
