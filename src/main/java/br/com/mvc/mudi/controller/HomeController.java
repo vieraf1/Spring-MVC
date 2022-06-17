e7d04e33723a10d85d43e997ea24212a31df41a2
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-06-16 21:08:21 -0300

    Usando JPA Puro com o Spring Data
    Usando JPA Puro com o Spring Data

--- src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,15 +15,14 @@ import br.com.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Nintendo Switch Lite Coral - Versão Nacional");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/61sfRoauZ8L._AC_SX385_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/dp/B09BDP1JXX?aaxitk=2d540e64d0589f9c42d1ad3edab674df&pd_rd_plhdr=t&smid=A1ZZFT5FULY4LN&ref=dacx_dp_584567158933768536_587232257304055682");		
-		pedido.setDescricao("nintendo switch");
+		Query query = entityManager.createQuery("Select p from Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
