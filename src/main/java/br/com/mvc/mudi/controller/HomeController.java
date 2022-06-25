cc4918a22b76ec818a33c323e8ab148418bd2b91
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-06-24 22:32:07 -0300

    colocando filtro por status na tela
    colocando filtro por status na tela

--- src/main/java/br/com/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/mvc/mudi/controller/HomeController.java
@@ -3,20 +3,37 @@ package br.com.mvc.mudi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.mvc.mudi.model.enums.StatusPedidoEnum;
 import br.com.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 	
 	@Autowired
 	private PedidoRepository repositoy;
 	
-	@GetMapping("/home")
+	@GetMapping
 	public String home(Model model) {
 		model.addAttribute("pedidos", repositoy.findAll());
 		return "home";
 	}
+	
+	@GetMapping("/{status}")
+	public String aguardando(@PathVariable("status") String status, Model model) {
+		model.addAttribute("pedidos", repositoy.findByStatus(
+				StatusPedidoEnum.valueOf(status.toUpperCase())));
+		model.addAttribute("status", status);
+		return "home";
+	}
 
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
+	}
 }
--- src/main/java/br/com/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/mvc/mudi/repository/PedidoRepository.java
@@ -1,10 +1,15 @@
 package br.com.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.mvc.mudi.model.Pedido;
+import br.com.mvc.mudi.model.enums.StatusPedidoEnum;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedidoEnum status);
 }
