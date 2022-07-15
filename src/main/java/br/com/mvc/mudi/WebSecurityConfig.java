5848ad52da448047a90813832ab355e3aad6dda2
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-07-14 22:29:35 -0300

    Criando tela de logout
    Criando tela de logout

--- src/main/java/br/com/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/mvc/mudi/WebSecurityConfig.java
@@ -22,7 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin(form -> form
 					.loginPage("/login")
 					.permitAll()
-			);
+			)
+			.logout(logout -> logout.logoutUrl("/logout"));
 	}
 	
 	@Bean
