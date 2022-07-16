ef04d8a3ed2ec9e3aef7ac4e8d83dfb41bad1832
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-07-15 22:15:48 -0300

    Usando JDBC Authentication
    Usando JDBC Authentication

--- src/main/java/br/com/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/mvc/mudi/WebSecurityConfig.java
@@ -1,19 +1,27 @@
 package br.com.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource datasource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -21,22 +29,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin(form -> form
 					.loginPage("/login")
+					.defaultSuccessUrl("/home", true)
 					.permitAll()
 			)
 			.logout(logout -> logout.logoutUrl("/logout"));
 	}
 	
-	@Bean
 	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user =
-				User.withDefaultPasswordEncoder()
-					.username("teste")
-					.password("1234")
-					.roles("ADM")
-					.build();
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
 		
-		return new InMemoryUserDetailsManager(user);
+		auth.jdbcAuthentication()
+			.dataSource(datasource)
+			.passwordEncoder(enconder);
 	}
 	
 }
