c01d1538f3bd7f522130363dca0ffcaeb5987784
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-07-21 21:16:03 -0300

    Alterando cadastro para informar o usuário que está realizando o procedimento
    Alterando cadastro para informar o usuário que está realizando o procedimento

--- src/main/java/br/com/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/mvc/mudi/controller/PedidoController.java
@@ -3,6 +3,7 @@ package br.com.mvc.mudi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.mvc.mudi.dto.RequisicaoNovoPedido;
 import br.com.mvc.mudi.model.Pedido;
+import br.com.mvc.mudi.model.User;
 import br.com.mvc.mudi.repository.PedidoRepository;
+import br.com.mvc.mudi.repository.UserRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
 	
 	@Autowired
-	private PedidoRepository repository;
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("formulario")
 	public String formulario(RequisicaoNovoPedido requisicao) {
@@ -31,8 +37,12 @@ public class PedidoController {
 			return "pedido/formulario";
 		}
 		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userRepository.findByUsername(username);
+		
 		Pedido pedido = requisicao.toPedido();
-		repository.save(pedido);
+		pedido.setUser(user);
+		pedidoRepository.save(pedido);
 		
 		return "redirect:/home";
 	}
