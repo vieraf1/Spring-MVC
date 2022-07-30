616e7c4c2f4243d3d6dfb9f873c1e65b23ed92b6
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-07-29 22:34:48 -0300

    Criando cadastro de ofertas
    Criando cadastro de ofertas

---
+++ src/main/java/br/com/mvc/mudi/api/OfertaRest.java
@@ -0,0 +1,39 @@
+package br.com.mvc.mudi.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mvc.mudi.dto.RequisicaoNovaOferta;
+import br.com.mvc.mudi.model.Oferta;
+import br.com.mvc.mudi.model.Pedido;
+import br.com.mvc.mudi.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/api/ofertas")
+public class OfertaRest {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@PostMapping
+	public Oferta criaOferta(RequisicaoNovaOferta requisicao) {
+		Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoId());
+		if(pedidoBuscado == null) {
+			return null;
+		}
+		
+		Pedido pedido = pedidoBuscado.get();
+		
+		Oferta novaOferta = requisicao.toOferta();
+		novaOferta.setPedido(pedido);
+		pedido.getOfertas().add(novaOferta);
+		pedidoRepository.save(pedido);
+		
+		return novaOferta;
+	}
+	
+}
